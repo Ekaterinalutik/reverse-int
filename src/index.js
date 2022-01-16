7cdbe6c42d96874f4fbd86070839cfc98b892ca9
kate.rodionova <ekaterinalutik@gmail.com>
2022-01-16 03:00:15 +0200

    feat: function created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    var number = Math.abs(n);
+    var nnum = number.toString();
+    var nStr = "";
+  for (var i=nnum.length-1; i >=0;i --) { 
+    nStr += nnum[i]}
+    return nStr;
 }
+
